adbb7a6ae86441b8e8378b44f9ee44f8a6457b73
Taehwan06 <98065293+Taehwan06@users.noreply.github.com>
2022-03-22 11:42:02 +0900

    Delete HomeFriends directory

--- HomeFriends/src/main/webapp/resources/js/mypage/payment1.js
+++
@@ -1,9 +0,0 @@
-function selectFn(obj){
-	var rInput = document.getElementById("requestInput");
-	
-	if(obj.value == "option01"){
-		rInput.style.visibility = "visible";
-	}else{
-		rInput.style.visibility = "hidden";
-	}
-}
