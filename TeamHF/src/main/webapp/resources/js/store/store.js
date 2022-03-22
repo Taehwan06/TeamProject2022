a76037a6e6aaa16432eb3bd0c6d989e71d077d7e
Gwonsiyun <49703174+Gwonsiyun@users.noreply.github.com>
2022-03-22 09:01:53 +0900

    del

--- TeamHF/src/main/webapp/resources/js/store/store.js
+++
@@ -1,7 +0,0 @@
-$(document).ready( function(){
-	//카테고리 클릭시 페이지 이동
-	$(".category_link").click(function(){
-		var id = $(this).attr('id');
-		location.href = 'store_list.jsp?searchType='+id;
-	});
-});
