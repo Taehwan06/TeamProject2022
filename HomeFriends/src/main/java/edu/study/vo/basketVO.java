c6c0743cc7e441ab1c40fc8309cd7be5dda7d5bd
MYCOM <MYCOM@DESKTOP-NAMEDPC>
2022-03-31 17:45:41 +0900

    페이지마다 header.js추가함 view페이지 컨텐츠이미지 및 장바구니 수정해야함

---
+++ HomeFriends/src/main/java/edu/study/vo/basketVO.java
@@ -0,0 +1,68 @@
+package edu.study.vo;
+
+public class basketVO {
+	private int sbidx;
+	private int midx;
+	private int spidx;
+	private String title;
+	private int cnt;
+	private String delivery_charge;
+	private String free_delivery;
+	private String img_origin;
+	
+	
+	
+	public int getSbidx() {
+		return sbidx;
+	}
+	public void setSbidx(int sbidx) {
+		this.sbidx = sbidx;
+	}
+	public int getMidx() {
+		return midx;
+	}
+	public void setMidx(int midx) {
+		this.midx = midx;
+	}
+	public int getSpidx() {
+		return spidx;
+	}
+	public void setSpidx(int spidx) {
+		this.spidx = spidx;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	public String getDelivery_charge() {
+		return delivery_charge;
+	}
+	public void setDelivery_charge(String delivery_charge) {
+		this.delivery_charge = delivery_charge;
+	}
+	public String getFree_delivery() {
+		return free_delivery;
+	}
+	public void setFree_delivery(String free_delivery) {
+		this.free_delivery = free_delivery;
+	}
+	public String getImg_origin() {
+		return img_origin;
+	}
+	public void setImg_origin(String img_origin) {
+		this.img_origin = img_origin;
+	}
+	
+	
+	
+
+	
+}
