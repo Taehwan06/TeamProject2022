136b471989e28a95a6109cdc1ef37c432673014b
lth-m <lth-mail@hanmail.net>
2022-04-12 14:17:11 +0900

    taehwan

---
+++ HomeFriends/src/main/java/edu/study/vo/PayInfoVO.java
@@ -0,0 +1,49 @@
+package edu.study.vo;
+
+public class PayInfoVO {
+	
+	private String impUid;
+	private String merchantUid;
+	private String paidAmount;
+	private String applyNum;
+	private String errorMsg;
+	private String successMsg;
+	
+	
+	public String getPaidAmount() {
+		return paidAmount;
+	}
+	public void setPaidAmount(String paidAmount) {
+		this.paidAmount = paidAmount;
+	}
+	public String getSuccessMsg() {
+		return successMsg;
+	}
+	public void setSuccessMsg(String successMsg) {
+		this.successMsg = successMsg;
+	}
+	public String getImpUid() {
+		return impUid;
+	}
+	public void setImpUid(String impUid) {
+		this.impUid = impUid;
+	}
+	public String getMerchantUid() {
+		return merchantUid;
+	}
+	public void setMerchantUid(String merchantUid) {
+		this.merchantUid = merchantUid;
+	}
+	public String getApplyNum() {
+		return applyNum;
+	}
+	public void setApplyNum(String applyNum) {
+		this.applyNum = applyNum;
+	}
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+}
